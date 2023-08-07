aa21a88c844afc0b55f2acc76e7e9c88ad58e4cb
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-08-07 17:06:36 +0300

    feat: add input and crate basic css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import React, { useState } from 'react';
+import './style.css';
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function addTodo() {
+    if (todo !== "") {
+      setTodos([todo, ...todos]);
+      setTodo("");
+    }
+  }
   return (
-    <div>
+    <div className="App">
+      <h1>ToDo App</h1>
+      <div className="input-container">
+        <input 
+          className="todo-input"
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="What needs to be done?" 
+        />
+        <button className="add-button" onClick={addTodo}>Add</button>
+      </div>
     </div>
   );
 }
